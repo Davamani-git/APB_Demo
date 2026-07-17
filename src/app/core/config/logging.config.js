765d17820ecb795a3be67393c075caefa0508bda
Davamani-git <davamani.a@ascendion.com>
2026-07-17 18:58:17 +0530

    Delete src/app directory

--- src/app/core/config/logging.config.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  'use strict';
-
-  angular.module('app.core')
-    .constant('loggingConfig', {
-      endpoint: '/client-logs',
-      redactFields: ['token', 'Authorization', 'cardNumber']
-    });
-}());
