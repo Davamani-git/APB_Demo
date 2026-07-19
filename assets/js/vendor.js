0655344fa3266a2c9d54bbf09b2ffcc5104b6ff1
Davamani-git <davamani.a@ascendion.com>
2026-07-19 19:54:59 +0530

    testimg

--- assets/js/vendor.js
+++
@@ -1,4 +0,0 @@
-(function () {
-    "use strict";
-    // Placeholder for allowed vendor utilities if needed.
-})();
--- mock/data/monthAvailability.mock.data.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    "use strict";
-
-    window.MonthAvailabilityMockData = [
-        { month: "2026-05", isCurrent: false, hasData: false },
-        { month: "2026-06", isCurrent: false, hasData: true },
-        { month: "2026-07", isCurrent: true, hasData: true }
-    ];
-})();
