9c6ae79a9084403b544097150a62bc5a882a5a93
Davamani-git <davamani.a@ascendion.com>
2026-08-12 16:40:44 +0530

    Delete src/app directory

--- src/app/app.module.js
+++
@@ -1,6 +0,0 @@
-(function(){
-'use strict';
-angular.module('onlineShoppingApp',['ngRoute','ecommerce.orders','ecommerce.sellerDashboard']);
-angular.module('ecommerce.orders',[]);
-angular.module('ecommerce.sellerDashboard',[]);
-})();
